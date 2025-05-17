439d13d795371ac50e415bb28519703e39f051cf
diablov <>
2025-05-17 16:06:22 +0800

    2025

---
+++ autoinstall.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+export APPROVE_INSTALL=y
+export APPROVE_IP=y
+export IPV6_SUPPORT=n
+export PORT_CHOICE=2
+export PORT=41194
+export PROTOCOL_CHOICE=1
+export DNS=11
+export COMPRESSION_ENABLED=n
+export CUSTOMIZE_ENC=n
+export CLIENT=soc
+export PASS=1
+export AUTO_INSTALL=y
+
+./openvpn-install.sh
